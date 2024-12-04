e9f78a0af65789bb547032ff973293098c45824f
FR13ND-ly <motricala44@gmail.com>
2024-12-04 15:09:55 +0200

    Upgrade ViewsManager

--- src/views/EditorView.hpp
+++ src/views/EditorView.hpp
@@ -11,19 +11,30 @@
 #include "../utils/WindowManager.hpp"
 #include "viewSwitcher.hpp"
 #include "../core/objects/Cube.hpp"
+#include "../core/objects/Sphere.hpp"
 
 class EditorView : public View {
 public:
-    EditorView() {
+    EditorView(): scene(nullptr) {
         sf::RenderWindow& window = const_cast<sf::RenderWindow&>(WindowManager::getInstance().getWindow());
         auto scene = std::make_shared<Scene>(window);
         scene->addObject(std::shared_ptr<Object3d>(new Cube(1.f)));
-
+        this->scene = scene;
         auto myButton = std::make_shared<Button>(
             sf::Vector2f(100, 100),
             sf::Vector2f(200, 50),
             "Home"
         );
+
+        auto sphereButton = std::make_shared<Button>(
+            sf::Vector2f(100, 300),
+            sf::Vector2f(200, 50),
+            "Sphere"
+        );
+
+        sphereButton->setOnClick([this]() {
+            this->scene->addObject(std::shared_ptr<Object3d>(new Sphere(1.f, 16, 16)));
+        });
 
         myButton->setOnClick([this]() {
             switchToView("home");
@@ -31,7 +42,10 @@ public:
 
         this->addComponent(scene);
         this->addComponent(myButton);
+        this->addComponent(sphereButton);
     }
+private:
+    std::shared_ptr<Scene> scene;
 };
 
 #endif
